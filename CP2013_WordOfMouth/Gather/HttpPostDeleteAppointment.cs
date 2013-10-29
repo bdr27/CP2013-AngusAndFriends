6ac89dc80a56c21c0d4566da77845a8cad050240
Brendan Russo <brendanrusso27@gmail.com>
2013-10-30 00:40:52 +1000

    HttpPost to delete appointment

---
+++ CP2013_WordOfMouth/Gather/HttpPostDeleteAppointment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CP2013_WordOfMouth.Utility;
+
+namespace CP2013_WordOfMouth.Gather
+{
+    public class HttpPostDeleteAppointment
+    {
+        private string baseUrl = HTTP_INFO.GetUrlBase();
+       // private string location = HTTP_INFO.Get
+    }
+}
