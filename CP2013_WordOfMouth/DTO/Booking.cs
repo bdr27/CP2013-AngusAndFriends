9f29ceac4de4ddca8a8b005172af3eac1bb4abec
Andrew Wickens <andrewjwickens@gmail.com>
2013-10-29 23:47:18 +1000

    Well shit.

---
+++ CP2013_WordOfMouth/DTO/Booking.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP2013_WordOfMouth.DTO
+{
+    public class Booking
+    {
+        private int sessionID;
+        private int appointmentType;
+        private int dentist;
+        private int day;
+        private string time;
+
+        public Booking(int sessionID, int appointmentType, int dentist, int day, string time)
+        {
+            this.sessionID = sessionID;
+            this.appointmentType = appointmentType;
+            this.dentist = dentist;
+            this.day = day;
+            this.time = time;
+        }
+
+        public int GetSessionID()
+        {
+            return sessionID;
+        }
+
+        public int GetAppointmentType()
+        {
+            return appointmentType;
+        }
+
+        public int GetDentist()
+        {
+            return dentist;
+        }
+
+        public int GetDay()
+        {
+            return day;
+        }
+
+        public string GetTime()
+        {
+            return time;
+        }
+    }
+}
---
+++ CP2013_WordOfMouth/JSON/ConverterBooking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP2013_WordOfMouth.JSON
+{
+    class ConverterBooking
+    {
+        public int seshID { get; set; }
+        public int appointment_type { get; set; }
+        public int dentist { get; set; }
+        public int day { get; set; }
+        public string time { get; set; }
+
+        public ConverterBooking(int seshID, int appointment_type, int dentist, int day, string time)
+        {
+            this.seshID = seshID;
+            this.appointment_type = appointment_type;
+            this.dentist = dentist;
+            this.day = day;
+            this.time = time;
+        }
+    }
+}
