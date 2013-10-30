a57179c53176101b5dbd12de334561cd772e8949
Brendan Russo <brendanrusso27@gmail.com>
2013-10-30 21:49:03 +1000

    Oh Baby Baby

---
+++ CP2013_Assignment Tests/JsonDentistEditAddTests.cs
@@ -0,0 +1,76 @@
+﻿using CP2013_WordOfMouth.DTO;
+using CP2013_WordOfMouth.Exceptions;
+using CP2013_WordOfMouth.JSON;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP2013_WordOfMouth_Tests
+{
+    [TestClass]
+    class JsonDentistEditAddTests
+    {
+        private string correctJson = "{\"dentistID\":123,\"name\":\"DENTIST NAME\",\"email\":\"example@domain.com\",\"phone\":\"044445556666\"}";
+        private int id = 123;
+        private string name = "DENTIST NAME";
+        private string email = "example@domain.com";
+        private string phone = "044445556666";
+
+        [TestMethod]
+        public void JsonDentistObjectToJsonTest()
+        {
+            TemplateJson tl = new JsonDentistEditAdd();
+            var json = tl.GetJson(new Dentist(id, name, email, phone));
+            Assert.AreEqual(correctJson, json);
+        }
+
+        [TestMethod]
+        public void JsonDentistJsonToObjectTest()
+        {
+            TemplateJson tl = new JsonDentistEditAdd();
+            var o = tl.GetObject(correctJson) as Dentist;
+            Assert.AreEqual(id, o.GetID());
+            Assert.AreEqual(name, o.GetName());
+            Assert.AreEqual(email, o.GetEmail());
+            Assert.AreEqual(phone, o.GetPhone());
+        }
+
+        [TestMethod]
+        public void JsonDentistObjectToJsonInvalidTest()
+        {
+            bool exception = false;
+            TemplateJson tl = new JsonDentistEditAdd();
+            try
+            {
+                var json = tl.GetJson("I am the wrong object");
+            }
+            catch (InvalidLoginObjectException)
+            {
+                exception = true;
+            }
+
+            Assert.IsTrue(exception);
+        }
+
+        [TestMethod]
+        public void JsonDentistJsonToObjectInvalidTest()
+        {
+            bool exception = false;
+            var incorrect = "{\"emails\":\"test.user@domain.com\",\"password\":\"Password\"}";
+            TemplateJson tl = new JsonDentistEditAdd();
+            try
+            {
+                var o = tl.GetObject(incorrect) as Dentist;
+            }
+            catch (InvalidLoginJsonException)
+            {
+                exception = true;
+            }
+
+            Assert.IsTrue(exception);
+        }
+    }
+}
---
+++ CP2013_WordOfMouth/JSON/JsonDentistEditAdd.cs
@@ -0,0 +1,43 @@
+﻿using CP2013_WordOfMouth.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP2013_WordOfMouth.JSON
+{
+    public class JsonDentistEditAdd : TemplateJson
+    {
+        public override string GetJson(object o)
+        {
+            var d = o as Dentist;
+            CheckNull(d);
+            return JsonConvert.SerializeObject(new ConverterDentistEditAdd(d.GetID(), d.GetName(), d.GetEmail(), d.GetPhone()));
+        }
+
+        public override object GetObject(string json)
+        {
+            var d = JsonConvert.DeserializeObject<ConverterDentistEditAdd>(json);
+            CheckValidParams(d.dentistID, d.name, d.email, d.phone);
+            return new Dentist(d.dentistID, d.name, d.email, d.phone);
+        }
+
+        private class ConverterDentistEditAdd
+        {
+            public int dentistID { get; set; }
+            public string name { get; set; }
+            public string email { get; set; }
+            public string phone { get; set; }
+
+            public ConverterDentistEditAdd(int dentistID, string name, string email, string phone)
+            {
+                this.dentistID = dentistID;
+                this.name = name;
+                this.phone = phone;
+                this.email = email;
+            }
+        }
+    }
+}
